d347d36d79a405707973587e4b73e12a486388c5
Noor5567 <83994879+Noor5567@users.noreply.github.com>
2022-04-24 21:53:27 +0300

    Add files via upload

---
+++ calc.php
@@ -0,0 +1,23 @@
+<?php
+function operation($opr)
+{
+if($opr=="+")
+{
+    echo 5+5;
+}
+else if($opr=="-")
+{
+    echo 10-5;
+}
+else if($opr=="*")
+{
+    echo 4*5;
+}
+else if($opr=="/")
+{
+    echo 20/4;
+}
+}
+operation("/");
+
+?>

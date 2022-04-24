7edad800271087567e4f6bd729555f2f33f3a15a
Noor5567 <83994879+Noor5567@users.noreply.github.com>
2022-04-24 22:18:22 +0300

    Add files via upload

---
+++ stringconv.php
@@ -0,0 +1,13 @@
+<?php
+function conv($string)
+{
+   echo strtoupper($string);
+   echo "<br>";
+   echo strtolower($string);
+   echo "<br>";
+   echo ucfirst($string);
+   echo "<br>";
+   echo ucwords($string);
+}
+conv("Fhjdn jskksjs iksjiskj")
+?>
